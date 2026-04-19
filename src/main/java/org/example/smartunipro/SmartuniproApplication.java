323e8a8c5ca2f5d4a6168aee311e76532c6e3300
kerolos119 <kerolosbotros99@gmail.com>
2026-04-19 14:09:00 +0200

    init

---
+++ src/main/java/org/example/smartunipro/SmartuniproApplication.java
@@ -0,0 +1,13 @@
+package org.example.smartunipro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartuniproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartuniproApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/smartunipro/SmartuniproApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.smartunipro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartuniproApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
